a65fa84b04f6864ae127f29e613dc6106edd9b2c
tvcosta <wvalentew@gmail.com>
2024-12-05 00:29:41 +0000

    US421 - Subscription rotate secret

--- src/Qala.Cli/Commands/Subscriptions/RotateWebhookSecretArgument.cs
+++ src/Qala.Cli/Commands/Subscriptions/RotateWebhookSecretArgument.cs
@@ -9,7 +9,7 @@ public class RotateWebhookSecretArgument : CommandSettings
     [Description("The name of the topic.")]
     public string TopicName { get; set; } = string.Empty;
 
-    [CommandOption("-s|--subscription-id <SUBSCRIPTION_ID>")]
+    [CommandOption("-s|--subscription <SUBSCRIPTION_ID>")]
     [Description("The id of the subscription.")]
     public Guid SubscriptionId { get; set; } = Guid.Empty;
 }
