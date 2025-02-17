ffa9b66b873eec58a4b7e4c8ac1765f71924493c
tvcosta <wvalentew@gmail.com>
2025-02-17 17:25:33 +0000

    New markdown documentation generation

--- src/Qala.Cli/Commands/GenerateMarkdown/GenerateMarkdownHandler.cs
+++ src/Qala.Cli/Commands/GenerateMarkdown/GenerateMarkdownHandler.cs
@@ -11,96 +11,200 @@ public record GenerateMarkdownRequest() : IRequest<Either<GenerateMarkdownErrorR
 
 public class GenerateMarkdownHandler : IRequestHandler<GenerateMarkdownRequest, Either<GenerateMarkdownErrorResponse, GenerateMarkdownSuccessResponse>>
 {
+    private const string XmlFileName = "generatedDocs/qala-cli-xmldoc.xml";
+    private const string MainMarkdownFile = "generatedDocs/qala-cli-docs.mdx";
+    private const string DocsFolder = "generatedDocs";
+    private static int sidebarPosition = 2;
+
     public Task<Either<GenerateMarkdownErrorResponse, GenerateMarkdownSuccessResponse>> Handle(GenerateMarkdownRequest request, CancellationToken cancellationToken)
     {
-        string xmlPath = "qala-cli-xmldoc.xml";
-        string markdownPath = "qala-cli-documentation.md";
+        if (!File.Exists(XmlFileName))
+        {
+            return Task.FromResult<Either<GenerateMarkdownErrorResponse, GenerateMarkdownSuccessResponse>>(new GenerateMarkdownErrorResponse("Error: XML file not found!"));
+        }
 
-        GenerateMarkdownFromXml(xmlPath, markdownPath);
+        Directory.CreateDirectory(DocsFolder);
+        XDocument doc = XDocument.Load(XmlFileName);
+
+        StringBuilder mainSb = new StringBuilder();
+        AddFrontMatter(mainSb, "qala-cli-docs", "Qala CLI Documentation",
+            "Qala CLI is a command-line interface that enables you to manage the Q-Flow solution via the terminal. It is suitable for integration into deployment pipelines or for automating certain tasks.",
+            ["qala", "q-flow", "documentation", "cli"], sidebarPosition++, false);
+
+        AddHeroBanner(mainSb, "Qala CLI Documentation", "Qala CLI is a command-line interface that enables you to manage the Q-Flow solution via the terminal.");
+        mainSb.AppendLine("## Available Commands\n");
+
+        mainSb.AppendLine("<table>");
+        mainSb.AppendLine("<tr><th>Command</th><th>Description</th></tr>");
+
+        if (doc.Root != null)
+        {
+            foreach (var command in doc.Root.Elements("Command"))
+            {
+                string commandName = command.Attribute("Name")?.Value ?? "Unknown";
+                string description = command.Element("Description")?.Value ?? "No description available.";
+                var subCommands = command.Elements("Command").ToList();
+                string fileName = $"qala-cli-{commandName}";
+
+                if (subCommands.Count != 0)
+                {
+                    mainSb.AppendLine($"<tr><td><a href=\"{fileName}\" title=\"Learn about {commandName}\">{commandName}</a></td><td>{description}</td></tr>");
+                    GenerateSubCommandMarkdown(command, $"{DocsFolder}/{fileName}", sidebarPosition++);
+                }
+                else
+                {
+                    mainSb.AppendLine($"<tr><td><a href=\"#{commandName.ToLower()}\" title=\"Learn about {commandName}\">{commandName}</a></td><td>{description}</td></tr>");
+                }
+            }
+
+            mainSb.AppendLine("</table>");
+
+            foreach (var command in doc.Root.Elements("Command"))
+            {
+                var subCommands = command.Elements("Command").ToList();
+                if (subCommands.Count == 0)
+                {
+                    mainSb.AppendLine(GenerateCommandMarkdown(command));
+                }
+            }
+
+            File.WriteAllText(MainMarkdownFile, mainSb.ToString());
+        }
+        else
+        {
+            return Task.FromResult<Either<GenerateMarkdownErrorResponse, GenerateMarkdownSuccessResponse>>(new GenerateMarkdownErrorResponse("Error: XML root element not found!"));
+        }
 
         return Task.FromResult<Either<GenerateMarkdownErrorResponse, GenerateMarkdownSuccessResponse>>(new GenerateMarkdownSuccessResponse());
     }
 
-    private static void GenerateMarkdownFromXml(string xmlPath, string markdownPath)
+    private static void GenerateSubCommandMarkdown(XElement command, string markdownPath, int position)
     {
-        if (!File.Exists(xmlPath))
-        {
-            Console.WriteLine("Error: XML file not found!");
-            return;
-        }
+        string commandName = command.Attribute("Name")?.Value ?? "Unknown";
+        string description = command.Element("Description")?.Value ?? "No description available.";
+        var subCommands = command.Elements("Command").ToList();
+        string fileName = $"qala-cli-{commandName}";
+        List<string> tags = ["qala", "q-flow", "documentation", "cli", commandName];
 
-        string xmlContent = File.ReadAllText(xmlPath);
-        xmlContent = xmlContent.Trim();
-        var doc = XDocument.Parse(xmlContent);
         var sb = new StringBuilder();
+        AddFrontMatter(sb, fileName, commandName, description, tags, position);
+        AddHeroBanner(sb, commandName, description);
 
-        sb.AppendLine("import HeroBanner from '../../src/components/HeroBanner/HeroBanner'");
-        sb.AppendLine();
-        sb.AppendLine("<HeroBanner");
-        sb.AppendLine("    title=\"Qala CLI Documentation\"");
-        sb.AppendLine("    tagline=\"Qala CLI is a command-line interface that enables you to manage the Q-Flow solution via the terminal. It is suitable for integration into deployment pipelines or for automating certain tasks, such as creating a new environment upon the onboarding of a new tenant.\"");
-        sb.AppendLine("    backgroundColor='transparent'");
-        sb.AppendLine("/>");
-        sb.AppendLine();
+        sb.AppendLine($"# `{commandName}` Commands\n");
 
-        if (doc.Root != null)
+        sb.AppendLine("<table>");
+        sb.AppendLine("<tr><th>Command</th><th>Description</th></tr>");
+
+        foreach (var subCommand in subCommands)
         {
-            ProcessCommands(doc.Root, sb, 2);
+            string subCommandName = subCommand.Attribute("Name")?.Value ?? "Unknown";
+            string subDescription = subCommand.Element("Description")?.Value ?? "No description available.";
+            var deeperSubCommands = subCommand.Elements("Command").ToList();
+            string subFileName = $"qala-cli-{subCommandName}";
+
+            if (deeperSubCommands.Count != 0)
+            {
+                sb.AppendLine($"<tr><td><a href=\"{subFileName}\" title=\"Learn about {subCommandName}\">{subCommandName}</a></td><td>{subDescription}</td></tr>");
+                GenerateSubCommandMarkdown(subCommand, $"{DocsFolder}/{subFileName}", position++);
+            }
+            else
+            {
+                sb.AppendLine($"<tr><td><a href=\"#{subCommandName.ToLower()}\" title=\"Learn about {subCommandName}\">{subCommandName}</a></td><td>{subDescription}</td></tr>");
+            }
         }
-        else
+
+        sb.AppendLine("</table>");
+
+        foreach (var subCommand in subCommands)
         {
-            Console.WriteLine("Error: XML root element not found!");
+            var deeperSubCommands = subCommand.Elements("Command").ToList();
+            if (deeperSubCommands.Count == 0)
+            {
+                sb.AppendLine(GenerateCommandMarkdown(subCommand));
+            }
         }
 
         File.WriteAllText(markdownPath, sb.ToString());
-        Console.WriteLine($"CLI documentation generated: {markdownPath}");
     }
 
-    private static void ProcessCommands(XElement element, StringBuilder sb, int headingLevel)
+    private static void AddFrontMatter(StringBuilder sb, string id, string title, string description, List<string> tags, int sidebarPosition, bool hideInSidebar = true)
     {
-        foreach (var command in element.Elements("Command"))
+        sb.AppendLine("---");
+        sb.AppendLine($"id: {id}");
+        sb.AppendLine($"title: {title}");
+        sb.AppendLine($"title_meta: {title}");
+        sb.AppendLine($"pagination_label: {title}");
+        sb.AppendLine($"keywords: [{string.Join(", ", tags)}]");
+        sb.AppendLine($"tags: [{string.Join(", ", tags)}]");
+        sb.AppendLine($"image: ../docs/img/logo_no_lettering.svg");
+        sb.AppendLine($"description: {description}");
+        sb.AppendLine($"sidebar_label: {title}");
+        sb.AppendLine($"sidebar_position: {sidebarPosition}");
+        sb.AppendLine($"draft: true");
+        sb.AppendLine($"hide_table_of_contents: false");
+        sb.AppendLine($"pagination_next: null");
+        sb.AppendLine($"pagination_prev: references/references-intro");
+        sb.AppendLine($"hide_title: true");
+        sb.AppendLine($"toc_max_heading_level: 2");
+        if (hideInSidebar)
         {
-            string commandName = command.Attribute("Name")?.Value ?? "Unknown";
-            string description = command.Element("Description")?.Value ?? "No description available.";
-
-            sb.AppendLine($"{new string('#', headingLevel)} `{commandName}`");
-            sb.AppendLine();
-            sb.AppendLine($"**Description:** {description}");
-            sb.AppendLine();
-
-            var parameters = command.Element("Parameters");
-            if (parameters != null && parameters.Elements("Option") != null)
-            {
-                sb.AppendLine("### Options:");
-                sb.AppendLine("| Option | Description |");
-                sb.AppendLine("|--------|-------------|");
-
-                foreach (var option in parameters.Elements("Option"))
-                {
-                    string optionTemplate = option.Attribute("Short") != null && !string.IsNullOrWhiteSpace(option.Attribute("Short")?.Value)
-                        ? $"`{option.Attribute("Short")?.Value}` / `{option.Attribute("Long")?.Value ?? "Unknown"}`"
-                        : $"`{option.Attribute("Long")?.Value}`" ?? "Unknown";
-                    string optionDesc = option.Element("Description")?.Value ?? "No description.";
-
-                    sb.AppendLine($"| {optionTemplate} | {optionDesc} |");
-                }
-                sb.AppendLine();
-            }
-
-            var examples = command.Element("Examples");
-            if (examples != null)
-            {
-                sb.AppendLine("### Examples:");
-                sb.AppendLine("```sh");
-                foreach (var example in examples.Elements("Example"))
-                {
-                    sb.AppendLine(example.Attribute("commandLine")?.Value ?? "No example provided.");
-                }
-                sb.AppendLine("```");
-                sb.AppendLine();
-            }
-
-            ProcessCommands(command, sb, headingLevel + 1);
+            sb.AppendLine("sidebar_class_name: hidden");
         }
+        sb.AppendLine("---\n");
+    }
+
+    private static string GenerateCommandMarkdown(XElement command)
+    {
+        var sb = new StringBuilder();
+        string commandName = command.Attribute("Name")?.Value ?? "Unknown";
+        string description = command.Element("Description")?.Value ?? "No description available.";
+
+        sb.AppendLine($"## `{commandName}`");
+        sb.AppendLine($"{description}\n");
+
+        var parameters = command.Element("Parameters");
+        if (parameters != null)
+        {
+            sb.AppendLine("**Options:**");
+            sb.AppendLine("<table>");
+            sb.AppendLine("<tr><th>Option</th><th>Description</th></tr>");
+
+            foreach (var option in parameters.Elements("Option"))
+            {
+                string optionTemplate = option.Attribute("Short") != null && !string.IsNullOrWhiteSpace(option.Attribute("Short")?.Value)
+                    ? $"`{option.Attribute("Short")?.Value}` / `{option.Attribute("Long")?.Value ?? "Unknown"}`"
+                    : $"`{option.Attribute("Long")?.Value}`" ?? "Unknown";
+                string optionDesc = option.Element("Description")?.Value ?? "No description.";
+                sb.AppendLine($"<tr><td>{optionTemplate}</td><td>{optionDesc}</td></tr>");
+            }
+
+            sb.AppendLine("</table>");
+        }
+
+        var examples = command.Element("Examples");
+        if (examples != null)
+        {
+            sb.AppendLine("**Examples:**");
+            sb.AppendLine("```sh");
+            foreach (var example in examples.Elements("Example"))
+            {
+                sb.AppendLine(example.Attribute("commandLine")?.Value ?? "No example provided.");
+            }
+            sb.AppendLine("```\n");
+        }
+
+        return sb.ToString();
+    }
+
+    private static void AddHeroBanner(StringBuilder sb, string commandName, string commandDescription)
+    {
+        sb.AppendLine("import HeroBanner from '../../src/components/HeroBanner/HeroBanner';");
+        sb.AppendLine();
+        sb.AppendLine($"<HeroBanner");
+        sb.AppendLine($"    title=\"{commandName}\"");
+        sb.AppendLine($"    tagline=\"{commandDescription}\"");
+        sb.AppendLine("    backgroundColor='transparent'");
+        sb.AppendLine("/>");
+        sb.AppendLine();
     }
 }
