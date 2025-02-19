eb93707fccd7b4d545c154316f099846472f6896
tvcosta <wvalentew@gmail.com>
2025-02-19 18:23:36 +0000

    ListTopicCommand Tests

--- tests/Qala.Cli.Integration.Tests/Commands/MockApi/ListTopicsCommandShould.cs
+++ tests/Qala.Cli.Integration.Tests/Commands/MockApi/ListTopicsCommandShould.cs
@@ -1,5 +1,5 @@
 using Moq;
-using Qala.Cli.Commands.EventTypes;
+using Qala.Cli.Commands.Topics;
 using Qala.Cli.Integration.Tests.Fixtures;
 using Spectre.Console;
 using Spectre.Console.Cli;
@@ -7,46 +7,49 @@ using Spectre.Console.Testing;
 
 namespace Qala.Cli.Integration.Tests.Commands.MockApi;
 
-public class ListEventTypesCommandShould(QalaCliBaseFixture fixture) : IClassFixture<QalaCliBaseFixture>
+public class ListTopicsCommandShould(QalaCliBaseFixture fixture) : IClassFixture<QalaCliBaseFixture>
 {
     private readonly IRemainingArguments _remainingArguments = new Mock<IRemainingArguments>().Object;
 
     [Theory]
-    [InlineData("events list")]
-    [InlineData("events ls")]
+    [InlineData("topics list")]
+    [InlineData("topics ls")]
     public async Task Execute_WithValidParameters(string input)
     {
         // Arrange
         var console = new TestConsole();
-        var command = new ListEventTypesCommand(fixture.Mediator, console);
+        var command = new ListTopicsCommand(fixture.Mediator, console);
         var arguments = input.Split(' ').ToList();
         var listIndex = arguments.IndexOf("list") != -1 ? arguments.IndexOf("list") : arguments.IndexOf("ls");
         var context = new CommandContext(arguments, _remainingArguments, arguments[listIndex], null);
         var expectedOutput = new TestConsole();
         expectedOutput.MarkupLine("Processing request...");
-        expectedOutput.MarkupLine("[green bold]Event Types:[/]");
+        expectedOutput.MarkupLine("[green bold]Topics:[/]");
         var grid = new Grid()
-            .AddColumns(4)
+            .AddColumns(5)
             .AddRow(
                 new Text("Id", new Style(decoration: Decoration.Bold)),
-                new Text("Type", new Style(decoration: Decoration.Bold)),
+                new Text("Name", new Style(decoration: Decoration.Bold)),
                 new Text("Description", new Style(decoration: Decoration.Bold)),
-                new Text("Content Type", new Style(decoration: Decoration.Bold))
+                new Text("Provisioning State", new Style(decoration: Decoration.Bold)),
+                new Text("Event Types", new Style(decoration: Decoration.Bold))
             );
 
-        foreach (var eventType in fixture.AvailableEventTypes)
+        foreach (var topic in fixture.AvailableTopics)
         {
             grid.AddRow(
-                new Text(eventType.Id.ToString()),
-                new Text(eventType.Type),
-                new Text(eventType.Description),
-                new Text(eventType.ContentType)
+                new Text(topic.Id.ToString()),
+                new Text(topic.Name),
+                new Text(topic.Description),
+                new Text(topic.ProvisioningState),
+                new Text(string.Join(", ", topic.EventTypes.Select(et => et.Type)))
             );
         }
+
         expectedOutput.Write(grid);
 
         // Act
-        var result = await command.ExecuteAsync(context, new ListEventTypesArgument());
+        var result = await command.ExecuteAsync(context, new ListTopicsArgument());
 
         // Assert
         Assert.Equal(0, result);
